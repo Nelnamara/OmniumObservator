a85eabfab54b277984cd96786170325d9b132fd6
Nelnamara <gwilkins@local>
2026-06-11 22:46:44 -0700

    Initial commit: OmniumObservator skeleton

---
+++ OmniumObservator.lua
@@ -0,0 +1,9 @@
+-- OmniumObservator
+-- Omnium Folio power system tracker for WoW Midnight 12.0.7+
+-- Author: Nelnamara
+
+OmniumObservator = {}
+local OO = OmniumObservator
+
+-- TODO: implement Folio progress tracking, upgrade path display,
+-- weekly cap monitoring once 12.0.7 API is available
